0be152193a24344abb1182770c5e20d2370cbc3a
adeotti <wafickachiambi@gmail.com>
2026-08-10 22:18:44 -0400

    update test file

--- lift_env/test.py
+++ lift_env/test.py
@@ -2,10 +2,9 @@ import warnings,logging
 warnings.filterwarnings("ignore")
 logging.disable(logging.CRITICAL)
 
-import torch,sys,robosuite
+import torch,robosuite
 from lift import Actor
 from robosuite.wrappers.gym_wrapper import GymWrapper
-from gymnasium.wrappers.stateful_observation import NormalizeObservation
 from robosuite import load_composite_controller_config
 
 controller = load_composite_controller_config(robot="Panda")
@@ -21,19 +20,13 @@ env = robosuite.make(
     control_freq = 20
 )
 env = GymWrapper(env,list(env.observation_spec()))
-#env = NormalizeObservation(env)
 
 obs = env.reset()[0]
 policy = Actor()
 
-checkpoint = torch.load("./39.pth",map_location="cpu",weights_only=False) 
+checkpoint = torch.load("./model.pth",map_location="cpu",weights_only=False) 
 policy.load_state_dict(checkpoint["actor state"])
 
-""" !!! when using that NormalizeObservation wrapper
-env.obs_rms.mean = checkpoint["obs_rms_mean"].numpy()
-env.obs_rms.var = checkpoint["obs_rms_var"].numpy()
-env.update_running_mean = True 
-"""
 for i in range(500*30):
     _,_,action = policy(torch.from_numpy(obs).float())
     obs,reward,done,trunc,info = env.step(action.detach().numpy())
