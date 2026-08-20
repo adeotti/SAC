178668997f4490c30eee6358814464f5f189a904
adeotti <wafickachiambi@gmail.com>
2026-08-20 00:43:22 -0400

    update networks

--- stack_env/networks.py
+++ stack_env/networks.py
@@ -1,10 +1,12 @@
 import torch 
 import torch.nn as nn 
 import torch.nn.functional as F
-from torch.distributions import Normal,Categorical
+from torch.distributions import Normal
 import numpy as np
 
-# observation dim = 162 and action dim = 9
+__all__ = ["HL","HL_Critic","LL","LL_Critic"]
+
+# observation dim = 162, action dim = 9 and goal dim = 6
 
 def weight_init(l):
     if isinstance(l,nn.Linear):
@@ -15,37 +17,58 @@ def weight_init(l):
 class HL(nn.Module):
     def __init__(self): # high level policy
         super().__init__()
-        goals = torch.tensor([
-            [1.0, 1.0], # reach
-            [1.0, 1.0], # lift
-            [1.0, 0.0]  # stack
-        ], dtype=torch.float32)
-        self.register_buffer("goal_table",goals)
-
+        # target is cat[gripper_to_cubeA,cubeA_pos], shape 6 
+        # to get the upper bound of the two values : gripper_to_cubeA.max() and cubeA_pos.max()  
         self.l1 = nn.Linear(162,256)
-        self.l2 = nn.Linear(256,256)
-        self.l3 = nn.Linear(256,3)
+        self.lmean = nn.Linear(256,6)
+        self.lstd = nn.Linear(256,6)
         self.apply(weight_init)
 
     def forward(self,obs):
         x = F.silu(self.l1(obs))
+
+        mean = self.lmean(x)
+        log_std = self.lstd(x)
+        log_std = torch.clamp(log_std,-20,2)
+        std = log_std.exp()
+        dist = Normal(mean,std) 
+        
+        pre_tanh = dist.rsample()
+        action = F.tanh(pre_tanh)
+        log = dist.log_prob(pre_tanh)
+        log -=  2 * (np.log(2) - pre_tanh - F.softplus(-2 * pre_tanh))  
+        log = log.sum(dim=-1,keepdim=True)  
+        return action,log,torch.tanh(mean)
+
+
+class HL_Critic(nn.Module): # high level critic 
+    def __init__(self):
+        super().__init__()
+        self.l1 = nn.Linear(162+6,256)
+        self.l2 = nn.Linear(256,256)
+        self.l3 = nn.Linear(256,256)
+        self.output = nn.Linear(256,1)
+        self.apply(weight_init) 
+
+    def forward(self,obs,goal): 
+        cat = torch.cat((obs,goal),dim=-1)
+        x = F.silu(self.l1(cat))
         x = F.silu(self.l2(x))
-        x = F.softmax(F.silu(self.l3(x)),-1)
-        idx = Categorical(x).sample()
-        return self.goal_table[idx] 
+        x = F.silu(self.l3(x))
+        return self.output(x)
 
 
 class LL(nn.Module): # low level policy
     def __init__(self):
         super().__init__()
-        self.l1 = nn.Linear(162+2,256) # -> obs + goal -> 256
+        self.l1 = nn.Linear(162+6,256) # -> obs + goal -> 256
         self.l2 = nn.Linear(256,256)
         self.l_mean = nn.Linear(256,9)
         self.l_std = nn.Linear(256,9)
         self.apply(weight_init)
         
     def forward(self,obs,goal):
-        x = torch.cat([obs,goal],dim=1)
+        x = torch.cat([obs,goal],dim=-1)
         x = F.silu(self.l1(x))
         x = F.silu(self.l2(x))
         
@@ -63,19 +86,18 @@ class LL(nn.Module): # low level policy
         return action,log,torch.tanh(mean)
     
 
-class Critic(nn.Module):
+class LL_Critic(nn.Module): # low level critic 
     def __init__(self):
         super().__init__()
-        self.l1 = nn.Linear(162 + 9,256)
+        self.l1 = nn.Linear(162+9+6,256) # -> obs + action + goal -> 256
         self.l2 = nn.Linear(256,256)
         self.l3 = nn.Linear(256,256)
         self.output = nn.Linear(256,1)
         self.apply(weight_init) 
 
-    def forward(self,obs,action): 
-        cat = torch.cat((obs,action),dim=-1)
+    def forward(self,obs,action,goal): 
+        cat = torch.cat((obs,action,goal),dim=-1)
         x = F.silu(self.l1(cat))
         x = F.silu(self.l2(x))
         x = F.silu(self.l3(x))
-        x = self.output(x)
-        return x
+        return self.output(x)
