1407cb580c08001ec660e6d6773e96654a54d94d
adeotti <wafickachiambi@gmail.com>
2026-09-13 16:02:14 -0400

    add high level policy frequency as a param

--- stack_env/configs.py
+++ stack_env/configs.py
@@ -13,21 +13,22 @@ class Hypers:
     ll_action_dim = 9  # low level action dim
     hl_action_dim = 6  # high level action dim
     batch_size = 1024
-    policy_lr = 3e-4 #.0001 
-    critic_lr = 3e-4 #.001
-    alpha_lr = 3e-4
+    policy_lr = 1e-4 
+    critic_lr = 1e-3
+    alpha_lr = 1e-4
     gamma = .99
-    tau = .005
+    tau = 5e-3
     max_hlp_update_steps = int(10e6) 
     max_llp_update_steps = int(10e6)
     num_envs = 10
     horizon = 500
-    buffer_size = 200  # 200*horizon = 100k steps
+    buffer_size = 100  # 100*horizon = 50k steps
     num_rollout_workers = 10
     warmup = 20_000 // (num_rollout_workers*num_envs)
     low_queue_maxsize = 10 
     high_queue_maxsize = 10
     buffer_min_capacity = 20 # min buffer capacity before starting sampling
+    c = 10 # frequency of the high level policy 
 
 hypers = Hypers()
 
